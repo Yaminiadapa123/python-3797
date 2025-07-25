acfd97aae52810fe87c2fe97676324ed8d4c84ea
Yaminiadapa123 <your-email@example.com>
2025-07-25 17:28:49 +0530

    if-statement

---
+++ 7.py
@@ -0,0 +1,62 @@
+#control statements
+''' 
+age=21 
+if age>=18:
+    print(f"{age} is eligible for vote")
+    ''' 
+''' num=123
+if num>0:
+    print(f"{num} is positive")
+    ''' 
+'''
+num=3
+
+if num%2==0:
+    print(f"{num} is even")
+if num%2!=0:
+    print(f"{num} is odd")
+
+
+            
+num=12
+if num%5==0:
+    print(f"{num} is divisible y 5")
+if num%5!=0:
+    print(f"{num} is not divisible by 5")
+    
+
+num=21
+if num%3==0 and num%7==0:
+    print(f"{num} is divisible by both 3 and 7")
+if num%3!=0 and num%7!=0:
+    print(f"{num}is not divisible by 3 and 7 ")
+    
+x=9
+y=67
+z=889
+if x>y and x>z:
+    print(f"{x} is largest one ")
+if y>x and y>z:
+    print(f"{y} is largest one ")
+if z>x and z>y:    
+    print(f"{z} is largest one")
+
+ 
+temp=8
+if temp>30:
+    print("hot temperature")
+if temp>=20 and temp<=30:
+    print("warm temperature")
+if temp<20:
+    print("cold temperature")
+
+grade=7
+if grade>=90:
+    print("Grade A")
+if grade>=75 and grade<=89:
+    print("Grade B")
+if grade>=50 and grade<=74:
+    print("Grade C")
+if grade<50:
+    print("Fail")
+'''
