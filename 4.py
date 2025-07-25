e2a9e3c6b48325b6082531cee57f6bca5a111131
Yaminiadapa123 <your-email@example.com>
2025-07-25 12:03:26 +0530

    operators

---
+++ 4.py
@@ -0,0 +1,79 @@
+#arithmetic operator
+#addition
+a=12
+b=8
+sum=a+b
+print(f"sum is {sum}")
+#subtraction
+a=27
+b=23
+sub=a-b
+print(f"subtraction is {sub}")
+#multiplication
+a=19
+b=9
+product=a*b
+print(f"product is {product}")
+#division
+a=2
+b=4
+division=a//b
+print(f"division is{division}")
+#modulus
+a=5
+b=50
+quotient=a%b
+print(f"quotient is {quotient}")
+#exponent
+a=6
+b=8
+result=a**b
+print(f"exponent is{result}")
+#comparison operator
+# eaual to
+x=11
+y=11
+z=x==y
+print(z)
+#not equal to
+x=7
+y=8
+z=x!=y
+print(z)
+#greater than
+x=123
+y=9764
+z=x>y
+print(z)
+#less than
+x=1234
+y=654
+z=x<y
+print(z)
+#greater than or equal
+x=87
+y=86
+z=x>=y
+print(z)
+#less than or equal
+x=87
+y=86
+z=x<=y
+print(z)
+#logical operators
+a=12
+b=13
+c=11
+result=a>b and a>c
+print(result)
+#assignment operators
+a=+5
+print(a)
+
+
+
+
+
+
+
+
