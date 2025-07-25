778b77f0f4c4186910d5c71ea76500b45c5f9e30
Yaminiadapa123 <your-email@example.com>
2025-07-25 11:04:29 +0530

    interpolation

--- 2.py
+++ 2.py
@@ -1,5 +1,14 @@
-brand_name='westside'
-product_price=1,299
-product_description="Westside Bombay Paisley Botanical Design A-Line Dress"
-prodct_size=[28,30,32]
-print(type(prodct_size),type(product_description),type(product_price))
+#brand_name='westside'
+#product_price=1,299
+#product_description="Westside Bombay Paisley Botanical Design A-Line Dress"
+#prodct_size=[28,30,32]
+#print(type(prodct_size),type(product_description),type(product_price))
+
+#x="python"
+#y="is"
+#z="programming language"
+#print(x+y+z)
+#used string concatenation
+x=10
+y="awesome"
+print(x,y)
---
+++ 3.py
@@ -0,0 +1,6 @@
+name="yamini"
+age=20
+new_age=1
+place="kakinada"
+college_name="kiet"
+print(f"my name is {name} im {age+new_age} years old im from {place} i completed my btech at {college_name} ")
